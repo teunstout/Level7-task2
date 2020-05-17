7f4f30b7fb381c3a8542ee935d7e13a598cdfc7c
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-05-17 12:11:44 +0200

    fragments

---
+++ hva_quest/app/src/main/java/com/example/hvaquest/LocationClueFragment.kt
@@ -0,0 +1,23 @@
+package com.example.hvaquest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LocationClueFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LocationClueFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location_clue, container, false)
+    }
+}
